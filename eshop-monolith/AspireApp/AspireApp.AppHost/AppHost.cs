18c03456b9748b9b21c3ca8bf7d368c3b9285215
renatocantarino <renato.cantarino@gmail.com>
2026-01-24 17:05:42 -0300

    Update aspire version to 9.0.5 to 13.0.1 Migrate from in-memory to PostgreSQL with EF Core Replaced in-memory database with a persistent PostgreSQL instance using Aspire and EF Core. Updated service and project references, added migration files for initial schema, and enabled database management via PgAdmin. This enables real data persistence and production-ready database support.

--- eshop-monolith/AspireApp/AspireApp.AppHost/AppHost.cs
+++ eshop-monolith/AspireApp/AspireApp.AppHost/AppHost.cs
@@ -1,8 +1,22 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+
+var postgresDb = builder.AddPostgres("postgres")
+                        .WithPgAdmin(admin => admin.WithUrlForEndpoint("http", url=> url.DisplayText = "PostgreDB Browser"))
+                        .WithDataVolume()
+                        .WithLifetime(ContainerLifetime.Persistent);
+
+var eshopDB = postgresDb.AddDatabase("eshopdb");
+                        
+                        
+                        
+
+
 builder
     .AddProject<Projects.WebApp>("webapp")
     .WithUrlForEndpoint("https", url => url.DisplayText = "EShop WebApp (HTTPS)")
-    .WithUrlForEndpoint("http", url => url.DisplayText = "EShop WebApp (HTTP)");
+    .WithUrlForEndpoint("http", url => url.DisplayText = "EShop WebApp (HTTP)")
+    .WithReference(eshopDB)
+    .WaitFor(eshopDB); ;
 
 builder.Build().Run();
